b0037219877165916165b5c7e33f9c87ec846f11
Jae Gangemi <jgangemi@gmail.com>
2018-04-19 18:45:48 -0600

    - renamed 'automate' command to 'repository' (#1)
    - refactored command to scan for json file based on just repository name

--- core/src/main/java/io/dangernoodle/grt/internal/WorkflowExecutor.java
+++ core/src/main/java/io/dangernoodle/grt/internal/WorkflowExecutor.java
@@ -1,6 +1,5 @@
 package io.dangernoodle.grt.internal;
 
-import java.io.File;
 import java.util.Collection;
 import java.util.Map;
 import java.util.function.Function;
@@ -10,34 +9,24 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Instance;
 import javax.enterprise.inject.Produces;
 
-import io.dangernoodle.grt.Arguments;
 import io.dangernoodle.grt.Repository;
 import io.dangernoodle.grt.Workflow;
 
 
 public class WorkflowExecutor
 {
-    private final String root;
-
     private final Map<String, Workflow> workflows;
 
-    WorkflowExecutor(String root, Collection<Workflow> workflows)
+    WorkflowExecutor(Collection<Workflow> workflows)
     {
-        this.root = root;
         this.workflows = workflows.stream()
                                   .collect(Collectors.toMap(workflow -> workflow.getName(), Function.identity()));
     }
 
-    public void execute(File file) throws Exception
+    public void execute(Repository repository) throws Exception
     {
-        Repository defaults = Repository.load(new File(root + File.separator + "configuration.json"));
-        Repository overrides = Repository.load(file);
-        
-        RepositoryMerger merger = new RepositoryMerger(defaults, overrides);
-        Repository repository = merger.merge();
-
         Workflow.Context context = new Workflow.Context();
-        
+
         for (String step : repository.getWorkflow())
         {
             if (!workflows.containsKey(step))
@@ -48,7 +37,6 @@ public class WorkflowExecutor
 
             workflows.get(step).execute(repository, context);
         }
-
     }
 
     @ApplicationScoped
@@ -56,9 +44,9 @@ public class WorkflowExecutor
     {
         @Produces
         @ApplicationScoped
-        public WorkflowExecutor get(Instance<Workflow> instance, Arguments arguments)
+        public WorkflowExecutor get(Instance<Workflow> instance)
         {
-            return new WorkflowExecutor(arguments.getRoot(), instance.stream().collect(Collectors.toList()));
+            return new WorkflowExecutor(instance.stream().collect(Collectors.toList()));
         }
     }
 }
