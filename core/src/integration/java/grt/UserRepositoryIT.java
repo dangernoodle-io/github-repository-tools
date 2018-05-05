5f2b80ba090c0929b769e7c862d22b8526e704a2
Jae Gangemi <jgangemi@gmail.com>
2018-05-05 17:08:45 -0600

    - added 'status check' only branch protection test

--- core/src/integration/java/grt/UserRepositoryIT.java
+++ core/src/integration/java/grt/UserRepositoryIT.java
@@ -2,15 +2,19 @@ package grt;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
 
+import org.apache.commons.lang.RandomStringUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.kohsuke.github.GHBranchProtection;
 import org.kohsuke.github.GHLabel;
 import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GitHub;
@@ -55,9 +59,19 @@ public class UserRepositoryIT
         context = new Workflow.Context();
         workflow = new GithubWorkflow(new GithubClient(github));
 
-        builder.setName("github-repository-tools-test")
+        // meh - the github api uses commons-lang, so...
+        builder.setName("github-repository-tools-test-" + RandomStringUtils.randomAlphanumeric(5).toLowerCase())
                .setOrganization(getOrganization())
                .setInitialize(true);
+    }
+
+    @Test
+    public void testBranchProtectionStatusChecksOnly() throws Exception
+    {
+        givenARepositoryWithStatusChecksOnly();
+        whenExecuteWorkflow();
+        thenRepositoryWasCreated();
+        thenStatusChecksOnlyWereConfigured();
     }
 
     @Test
@@ -79,6 +93,12 @@ public class UserRepositoryIT
         builder.addLabel("skip-build", Color.from("#006b75"));
     }
 
+    private void givenARepositoryWithStatusChecksOnly()
+    {
+        builder.requireBranchUpToDate("master", true)
+               .addRequiredContext("master", "context");
+    }
+
     private void thenLabelsWereConfigured() throws IOException
     {
         GHLabel label = ghRepo.getLabel("skip-build");
@@ -90,6 +110,19 @@ public class UserRepositoryIT
         assertThat(ghRepo, notNullValue());
         assertThat(ghRepo.getName(), equalTo(repository.getName()));
         assertThat(ghRepo.getOwnerName(), equalTo(getOrganization()));
+    }
+
+    private void thenStatusChecksOnlyWereConfigured() throws IOException
+    {
+        GHBranchProtection protection = ghRepo.getBranch("master").getProtection();
+
+        assertThat(protection.getRestrictions(), nullValue());
+        assertThat(protection.getRequiredReviews(), nullValue());
+        assertThat(protection.getEnforceAdmins().isEnabled(), equalTo(false));
+
+        assertThat(protection.getRequiredStatusChecks(), notNullValue());
+        assertThat(protection.getRequiredStatusChecks().isRequiresBranchUpToDate(), equalTo(true));
+        assertThat(protection.getRequiredStatusChecks().getContexts(), hasItem("context"));
     }
 
     private void whenExecuteWorkflow() throws IOException
