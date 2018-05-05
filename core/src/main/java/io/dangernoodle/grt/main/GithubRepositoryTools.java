a27de1883db8d093e3c6100b7f94459cb6bfc854
Jae Gangemi <jgangemi@gmail.com>
2018-05-04 18:09:23 -0600

    - catch and log any unexpected exceptions

--- core/src/main/java/io/dangernoodle/grt/main/GithubRepositoryTools.java
+++ core/src/main/java/io/dangernoodle/grt/main/GithubRepositoryTools.java
@@ -2,6 +2,8 @@ package io.dangernoodle.grt.main;
 
 import org.jboss.weld.environment.se.Weld;
 import org.jboss.weld.environment.se.WeldContainer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.dangernoodle.grt.cli.CommandLineDelegate;
 import io.dangernoodle.grt.cli.CommandLineDelegate.Executor;
@@ -9,16 +11,28 @@ import io.dangernoodle.grt.cli.CommandLineDelegate.Executor;
 
 public class GithubRepositoryTools
 {
+    private static final Logger logger = LoggerFactory.getLogger(GithubRepositoryTools.class);
+
     public static void main(String... args) throws Exception
     {
         Weld weld = new Weld();
         WeldContainer container = weld.initialize();
 
-        container.select(getCommandClass(container, args))
-                 .get()
-                 .execute();
-
-        container.shutdown();
+        try
+        {
+            container.select(getCommandClass(container, args))
+                     .get()
+                     .execute();
+        }
+        catch (Exception e)
+        {
+            // catch and log any exceptions that make it this far so we shutdown gracefully
+            logger.error("an unexpected error has occurred", e);
+        }
+        finally
+        {
+            container.shutdown();
+        }
     }
 
     private static Class<? extends Executor> getCommandClass(WeldContainer container, String... args)
