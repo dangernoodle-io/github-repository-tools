6b019b2b3576354e0f7cc22a76407a1d80b72b28
Jae Gangemi <jgangemi@gmail.com>
2022-03-13 08:01:00 -0600

    - wip

--- src/test/java/io/dangernoodle/grt/util/GithubRepositoryToolsUtilsTest.java
+++ src/test/java/io/dangernoodle/grt/util/GithubRepositoryToolsUtilsTest.java
@@ -1,7 +1,12 @@
 package io.dangernoodle.grt.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
+
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.security.PrivateKey;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -25,9 +30,13 @@ public class GithubRepositoryToolsUtilsTest
     }
 
     @Test
-    public void testToSha1()
+    public void testReadPrivateKey() throws Exception
     {
-        assertEquals("3ac68f502267", GithubRepositoryToolsUtils.toSha1(mockCommit));
+        Reader reader = new InputStreamReader(getClass().getResourceAsStream("/dummy-key.pem")); 
+        PrivateKey key = GithubRepositoryToolsUtils.readPrivateKey(reader);
+        
+        assertNotNull(key);
+        assertEquals("PKCS#8", key.getFormat());
     }
 
     @Test
@@ -35,4 +44,12 @@ public class GithubRepositoryToolsUtilsTest
     {
         assertEquals("746f486578", GithubRepositoryToolsUtils.toHex("toHex".getBytes()));
     }
+    
+    @Test
+    public void testToSha1()
+    {
+        assertEquals("3ac68f502267", GithubRepositoryToolsUtils.toSha1(mockCommit));
+    }
+
+    
 }
